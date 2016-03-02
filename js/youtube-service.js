8526f78a1053f2b3c02b5635c12162b693421332
soeun <mojosoeun@gmail.com>
2016-03-02 23:55:51 +0900

    youtube playlist로 변환

--- js/youtube-service.js
+++ js/youtube-service.js
@@ -6,7 +6,7 @@
         service.youtube = null;
 
         service.getYoutube = function(search_term){
-          return $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q="+search_term+"&key="+YOUTUBE_API_KEY).
+          return $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q="+search_term+"&key="+YOUTUBE_API_KEY + "&maxResults=30&&type=playlist").
               then(function(response) {
                   service.youtube = response.data;
                   console.log(service.youtube);
@@ -17,12 +17,25 @@
         service.getVideoId = function(){
           var videoId;
           if(service.youtube.items.length > 0 ){
-            videoId = service.youtube.items[0].id.videoId;
+            var r = Math.floor((Math.random() * service.youtube.items.length) + 1);
+            videoId = service.youtube.items[r].id.videoId;
             return videoId;
           }
           //return default youtube
           return "M7lc1UVf-VE";
         };
+
+        service.getPlaylistId = function(){
+          var playlitId;
+          if(service.youtube.items.length > 0 ){
+            var r = Math.floor((Math.random() * service.youtube.items.length) + 1);
+            playlitId = service.youtube.items[r].id.playlistId;
+            return playlitId;
+          }
+          //return default youtube
+          return "M7lc1UVf-VE";
+        };
+        
         return service;
     }
 
