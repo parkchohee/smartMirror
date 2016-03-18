dc1cdc4acbe2e4895dcf06d923f3a791478bd124
soeun <mojosoeun@gmail.com>
2016-03-18 22:38:48 +0900

    change config.js location

--- js/config.js
+++
@@ -1,5 +0,0 @@
-var FORCAST_API_KEY = "{API_KEY}";
-var SUBWAY_API_KEY = "{API_KEY}";
-var SOUNDCLOUD_APT_KEY = "{API_KEY}";
-var YOUTUBE_API_KEY = "{API_KEY}";
-var HUE_BASE = "http://192.168.0.36/api/{API_KEY}/";
